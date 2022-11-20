efcf0bfca1228e53237128758dadff92a7a9dfbf
Phupa S <99344705+PhupaSirirat@users.noreply.github.com>
2022-11-21 01:35:44 +0700

    Heap

---
+++ LAB11/src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+
+//        ref จากหนังสือ ศรันญา มณีโรจน์
+        int[] a = {0,   20,9,32,6,15,25,50,5,8,13,18};
+        int numNode = a.length-1;
+        Heap h = new Heap();
+        System.out.print("Data Array: ");
+        h.printHeap(a, numNode);
+        h.buildHeap(a, numNode);
+        System.out.print("Heap Array: ");
+        h.printHeap(a, numNode);
+    }
+}
